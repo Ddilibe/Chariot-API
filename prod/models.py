49e5f0eb35d6a9e0fba332b27434e75eb83c873d
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-03-16 10:59:51 +0100

    [Chore]: Manipulated the models for easy use

--- prod/models.py
+++ prod/models.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """ Script for the model of product for the ecommerce API """
+import json
+import uuid
+import base64
 from . import prod
-from run import db
+from os.path import join
+from run import db, TEMP_PATH
 from sqlalchemy_media import Image, ImageAnalyzer, ImageValidator, ImageProcessor
 
 
@@ -13,32 +17,71 @@ class ProductImage(Image):
             maximum=(800, 600),
             content_types=['image/jpeg', 'image/png']
         ),
-        ImageProcessor
+        ImageProcessor(
+            fmt='jpeg',
+            width=120
+        )
     ]
 
 
-product_tags = db.Table("product_tags", db.Model.metadata,
-    db.Column('tags_id', db.Unicode, db.ForeignKey("Tags.id")),
-    db.Column('Product_id', db.Unicode, db.ForeignKey("Product.id")),
+class Json(db.TypeDecorator):
+    impl = db.Unicode
+
+    def process_bind_param(self, value, engine):
+        return json.dumps(value)
+
+    def process_result_value(self, value, engine):
+        return json.loads(value) if value else None
+
+
+product_tags = db.Table("product_tag", db.Model.metadata,
+    db.Column('Product_id', db.Unicode, db.ForeignKey("product.prod_id")),
+    db.Column('Tag_id', db.Unicode, db.ForeignKey("tag.tag_id"))
 )
 
 class Product(db.Model):
-    __tablename__ = "Product"
+    __tablename__ = "product"
 
-    product_id = db.Column(db.Unicode(length=225), primary_key=True, unique=True)
+    prod_id = db.Column(db.Unicode(length=225), primary_key=True, unique=True)
     name = db.Column(db.String(128), nullable=False)
     description = db.Column(db.Text)
     price = db.Column(db.Integer, nullable=False)
     in_stock = db.Column(db.Integer, default=0)
     merchant_id = db.Column(db.Integer, db.ForeignKey('User.merchant_id'))
-    tags = db.relationship("Tags", secondary=table, back_populates="Product")
-    # image =
+    tags = db.relationship("Tag", secondary=product_tags, back_populates="products")
+    image = db.Column(ProductImage.as_mutable(Json))
     # category =
 
-class Tags(db.Model):
-    __tablename__ = "Tags"
+    def __init__(self, *args,  **kwargs):
+        super(Product, self). __init__( *args, **kwargs)
+        self.prod_id = str(uuid.uuid4())
+
+    @classmethod
+    def get_dict(cls):
+        # with open(self.)
+        # with open(TEMP_PATH, self.image.path) as im:
+        # image = self.image.get_thumbnail(ratio=3, auto_generate=True)
+        return cls.__dict__
+
+    def get(self, value):
+        return self.__dict__.get(value)
+
+
+class Tag(db.Model):
+    __tablename__ = "tag"
 
     tag_id = db.Column(db.Unicode(length=224), primary_key=True, unique=True)
     name = db.Column(db.String(128), nullable=False)
-    product = db.relationship("Product", secondary=table, back_populates="Tags")
+    products = db.relationship("Product", secondary=product_tags, back_populates="tags")
     descriiption = db.Column(db.Text)
+
+    def __init__(self, *args, **kwargs):
+        # super(db.Model, self).__init__(**kwargs)
+        super(Tag, self).__init__(*args, **kwargs)
+        self.tag_id = str(uuid.uuid4())
+
+    def get_tags_options(self):
+        return ["tag_id", "name", "products", "descriiption"]
+
+    def get(self, value):
+        return self.__dict__.get(value)
