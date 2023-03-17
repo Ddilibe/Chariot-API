21d8e18ffd511fd866fc19bcae763dcaca8e17bb
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-03-17 20:13:55 +0100

    [Feat]: Started implementing the cart feature

---
+++ cart/models.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Script for the models for the cart """
+import uuid
+from . import cart
+from run import db
+from auth.models import User
+from prod.models import Product
+from utils.exception import NotProductInstance
+
+
+class Cart(db.model):
+    __tablename__ = 'cart'
+
+    id = db.String(db.Unicode, primary_key=True)
+    user_id = db.mapped_column(db.ForeignKey("User.id"))
+    user = db.relationship('User', back_populates='cart')
+    products = []
+
+    def __init__(self, *args, **kwargs):
+        super(Cart, self).__init__(*args, **kwargs)
+        self.id = str(uuid.uuid4())
+
+    def add_item(self, value):
+        if isinstance(Product, value['name']):
+            self.products.append(value)
+            return
+        raise NotProductInstance
+
+    def remove_item(self, value):
+        if isinstance(Product, value):
+            self.product.remove(value)
+            return
+        raise NotProductInstance
+
+    def display_item(self):
+        info, y = {"Products": {}}, 0
+        for prod in self.products:
+            y += 1
+            info['Products'][y] = {}
+            for value in ['name', "description", "price", "image"]:
+                info['Products'][y][value] = prod['name'].get(value)
+        return info
