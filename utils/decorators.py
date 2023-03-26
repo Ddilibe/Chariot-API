963c0359f718c2e3db20ec2318e2ac8c3b8d5b61
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-03-26 10:17:06 +0100

    [Chore]: Made a few changes to the utility module

--- utils/decorators.py
+++ utils/decorators.py
@@ -3,15 +3,20 @@
 from run import redis_cli
 from functools import wraps
 from .exception import UserNotLoggedIn
+from auth.models import User
 
 
-def confirm_login(func):
-    @wraps(func)
-    def wrapper(user_id, *args, **kwargs):
-        email = (redis_cli.get(user_id)).decode('utf-8')
-        if not email:
-            return UserNotLoggedIn("User with the input not logged in")
-        else:
-            return func(*arg, **kwargs)
-    return wrapper
+def user_has_loggedin(user_id):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            email = redis_cli.get(user_id)
+            if not email:
+                raise UserNotLoggedIn("User Has Not LoginIn")
+            email = email.decode("utf-8")
+            user = User.query.filter_by(email_address=email).first_or_404()
+            kwargs['user'] = user
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
